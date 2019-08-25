b078e1e5e54f1d9328cdcdbfa5feb0482dd8d9a1
Anakin24 <mdivani1@mail.com>
2019-08-25 09:29:45 -0700

    reduux

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const About  = () => {
+    return(
+        <div className="container">
+            <h4 className="center">
+                About
+            </h4>
+            <p>
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
+            </p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Contact  = () => {
+    return(
+        <div className="container">
+            <h4 className="center">
+                Contact
+            </h4>
+            <p>
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
+            </p>
+        </div>
+    )
+}
+
+export default Contact;
