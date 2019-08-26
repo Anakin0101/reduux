7b3fb63a5af4e431cbc7f6a916357a3914ce2dbf
Anakin0101 <mdivani1@mail.com>
2019-08-26 17:39:26 +0400

    redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React , {Component} from 'react';
 import axios from "axios";
-import { Link } from "react";
+import { Link } from "react-router-dom";
+import Pokeball from "../pokeball.png"
 
 
 
 export default class Home extends Component {
     state= {
-        posts :  {}
+        posts :  []
     }
     componentDidMount() {
         axios.get("https://jsonplaceholder.typicode.com/posts")
@@ -22,6 +23,7 @@ export default class Home extends Component {
         const postList = posts.length ? (posts.map(post => {
             return(
                 <div className="post card" key ={post.id}>
+                    <img src={Pokeball} alt="A pokeball"/>
                     <div className="card-content">
                         <Link to={"/" + post.id}>
                         <span className="card-title">{post.title}</span>
